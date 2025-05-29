a4f7f3432c88266c788e060d89ab712bd5d42c87
pyniuX <krzysiek01pk@gmail.com>
2025-05-29 15:34:51 +0200

    environment init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2025 Krzysztof Puk
+// All rights reserved
+// https://github.com/pyniuX
+
+namespace pokemon;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
